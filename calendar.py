7f8787727208579b0634572138380b85115f3e2c
malavikasanthosh <77485867+malavikasanthosh@users.noreply.github.com>
2021-02-01 11:49:38 +0530

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,10 @@
+import calendar as cal
+from datetime import *
+print(cal.month(2021,2))
+d=datetime.today()
+print('Year:',d.year)
+print('Month:',d.month)
+print('Hour:',d.hour)
+print('Minute:',d.mintue)
+print('Second:',d.second)
+print('Date Time Format:',d)
